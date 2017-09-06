41c74c1c54b4d81e1d92261007bbfc3d5c75b3d2
Jorg Are <jorg2all@yahoo.com>
2017-09-06 13:01:57 +0100

    create custom bad request func

--- utils/error.go
+++ utils/error.go
@@ -1,6 +1,20 @@
 package utils
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Error struct to return a json error
 type Error struct {
 	Error string `json:"error"`
+}
+
+func BadRequest(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+	error := Error{
+		Error: err.Error(),
+	}
+	json.NewEncoder(w).Encode(error)
 }
