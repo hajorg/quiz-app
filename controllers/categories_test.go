2787375c655750c762ff0d39fc8deb36b929c9cf
Jorg Are <jorg2all@yahoo.com>
2017-10-30 19:32:54 +0100

    Controller get function (#6)
    * add test for get category controller
    
    * implement getWhere model method

--- controllers/categories_test.go
+++ controllers/categories_test.go
@@ -1,6 +1,7 @@
 package controllers_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"quiz-app/controllers"
@@ -26,6 +27,44 @@ func TestCreateCategorySuccess(t *testing.T) {
 
 	if status := res.Code; status != http.StatusCreated {
 		t.Errorf("Error occurred. Expected %v but got %v status code", http.StatusCreated, status)
+	}
+
+	_, err = db.Exec("DROP DATABASE IF EXISTS quiztest")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func TestGetCategorySuccess(t *testing.T) {
+	db := utils.DbTestInit()
+	_, err := db.Exec("INSERT INTO categories(id, title, description) VALUES(1, 'general', 'General stuff')")
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "category", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(controllers.GetCategories)
+	handler.ServeHTTP(res, req)
+
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf("Error occurred. Expected %v but got %v status code", http.StatusOK, status)
+	}
+	var resBody interface{}
+	json.Unmarshal(res.Body.Bytes(), &resBody)
+	result, ok := resBody.([]interface{})
+	if !ok {
+		panic("Error occurred")
+	}
+
+	if length := len(result); length != 1 {
+		t.Errorf("Error occurred. Expected length of %v but got %v ", 1, length)
 	}
 
 	_, err = db.Exec("DROP DATABASE IF EXISTS quiztest")
--- database/model.go
+++ database/model.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 )
 
-// Connect connects to the local database
 var dbName string
 
 func init() {
@@ -19,6 +18,7 @@ func init() {
 	}
 }
 
+// Connect connects to the local database
 func Connect(name string) *sql.DB {
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/"+name)
 	if err != nil {
@@ -31,6 +31,7 @@ func Connect(name string) *sql.DB {
 	return db
 }
 
+// Insert inserts into a table
 func Insert(table string, data map[string]interface{}) (int64, error) {
 	var newData []map[string]interface{}
 
@@ -74,6 +75,7 @@ func Insert(table string, data map[string]interface{}) (int64, error) {
 	return lastID, nil
 }
 
+// GetAll gets all data for a paticular table
 func GetAll(table string) []map[string]interface{} {
 	db := Connect(dbName)
 	defer db.Close()
@@ -90,7 +92,6 @@ func GetAll(table string) []map[string]interface{} {
 		panic(err)
 	}
 	// placeholder of result from the database
-	// data := []interface{}{}
 	data := make([]interface{}, len(columns))
 	// an array of the results to be gotten from the db
 	newData := make([]map[string]interface{}, 0)
@@ -112,7 +113,86 @@ func GetAll(table string) []map[string]interface{} {
 				if ok {
 					entry[col] = string(val)
 				} else {
-					entry[col] = val
+					entry[col] = data[i]
+				}
+				// convert from interface to string, float, boolean etc
+				switch value := entry[col].(type) {
+				case string:
+					if boolean, err := strconv.ParseBool(value); err == nil {
+						entry[col] = boolean
+					}
+
+					if val, err := strconv.ParseFloat(value, 64); err == nil {
+						entry[col] = val
+					}
+				}
+			}
+			newData = append(newData, entry)
+		} else {
+			break
+		}
+	}
+
+	return newData
+}
+
+// GetWhere gets all data from a table where it meets the `where` condition
+func GetWhere(table string, where []map[string]interface{}) []map[string]interface{} {
+	db := Connect(dbName)
+	defer db.Close()
+
+	whereCount := len(where)
+	whereValues := []interface{}{}
+	whereCondition := "SELECT * FROM " + table + " WHERE"
+	for idx, arr := range where {
+		for key, value := range arr {
+			whereCondition += " " + key + " = " + "?"
+			whereValues = append(whereValues, value)
+			if idx != whereCount-1 {
+				whereCondition += " AND"
+			}
+		}
+	}
+
+	stmt, err := db.Prepare(whereCondition)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(whereValues...)
+	if err != nil {
+		panic(err)
+	}
+
+	// get table columns
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err)
+	}
+	// placeholder of result from the database
+	data := make([]interface{}, len(columns))
+	// an array of the results to be gotten from the db
+	newData := make([]map[string]interface{}, 0)
+	// holds the address of each interface{} value in the data slice
+	scanArgs := make([]interface{}, len(columns))
+	for idx := range data {
+		scanArgs[idx] = &data[idx]
+	}
+
+	for {
+		if rows.Next() {
+			err = rows.Scan(scanArgs...)
+			if err != nil {
+				panic(err)
+			}
+			entry := make(map[string]interface{})
+			for i, col := range columns {
+				val, ok := data[i].([]byte)
+				if ok {
+					entry[col] = string(val)
+				} else {
+					entry[col] = data[i]
 				}
 				// convert from interface to string, float, boolean etc
 				switch value := entry[col].(type) {
